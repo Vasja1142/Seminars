06fc88d52a7507d3863b018824390848bd26f857
Nikitos <vasja11420@gmail.com>
2023-01-28 14:08:53 +0500

    Еще програмка

--- algoritm/Program.cs
+++ algoritm/Program.cs
@@ -1,58 +1,12 @@
 ﻿Console.Clear();
 
-Console.WriteLine("Если захотите выйти введите 100");
+Console.WriteLine("Введите трехзначное число: ");
 
 int num;
-while(true){
-    Console.Write("Введите номер дня недели: ");
-    num = int.Parse(Console.ReadLine()!);
-    // if(num == 1){
-    //     Console.WriteLine("Понедельник"); 
-    // }
-    // else if (num == 2){
-    //     Console.WriteLine("Вторник"); 
-    // }
-    // else if (num == 3){
-    //     Console.WriteLine("Среда"); 
-    // }
-    // else if (num == 4){
-    //     Console.WriteLine("Четверг"); 
-    // }
-    // else if (num == 5){
-    //     Console.WriteLine("Пятница");
-    // }
-    // else if (num == 6){
-    //     Console.WriteLine("Суббота");  
-    // }
-    // else if (num == 7){
-    //     Console.WriteLine("Воскресенье");  
-    // }
-    
-    switch (num){
-        case 1:
-            Console.WriteLine("Понедельник"); 
-            break;
-        case 2:
-            Console.WriteLine("Вторник");  
-            break;
-        case 3:
-            Console.WriteLine("Среда");  
-            break;
-        case 4:
-            Console.WriteLine("Четверг");  
-            break;
-        case 5: 
-            Console.WriteLine("Пятница"); 
-            break;
-        case 6:
-            Console.WriteLine("Суббота");  
-            break;
-        case 7:
-            Console.WriteLine("Воскресенье");  
-            break;
-        default: 
-            Console.WriteLine("Не знаю такого дня недели!");
-            break;
-    }
-    if(num == 100) break; 
-}
+num = int.Parse(Console.ReadLine()!);
+
+int B = num / 10;
+
+int num1 = num - B * 10;
+
+Console.Write($"{num1} ");
